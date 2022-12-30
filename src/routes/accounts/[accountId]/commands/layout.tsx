b80f159761c7172302b0ebe183e79431f908e8af
LachlanCourt <lachlan.m.court@gmail.com>
2022-12-30 21:29:03 +1100

    Fix layout issue

--- src/routes/accounts/[accountId]/commands/layout.tsx
+++ src/routes/accounts/[accountId]/commands/layout.tsx
@@ -1,13 +1,13 @@
 import { component$, Slot } from "@builder.io/qwik";
 import { RequestHandler } from "@builder.io/qwik-city";
-import Header from "../components/header/header";
+import Header from "~/components/header/header";
 
 interface Response { someData: string }
 
-export const onRequest: RequestHandler<Response> = ({ response, request, cookie }) => {
-  // console.log('Hit')
-  return { someData: 'hello' }
-}
+// export const onRequest: RequestHandler<Response> = ({ response, request, cookie }) => {
+//   console.log('Hit')
+//   return { someData: 'hello' }
+// }
 
 export default component$(() => {
   console.log('hi there')
