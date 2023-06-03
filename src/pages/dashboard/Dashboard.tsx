126e28476f173bb0c0db06874c1162e8af6b32d2
LachlanCourt <lachlan.m.court@gmail.com>
2023-06-03 17:58:17 +1000

    Update dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,11 +1,12 @@
 import { component$ } from "@builder.io/qwik";
+import { Button } from "~/components/button";
+import { Heading } from "~/components/heading/Heading";
+import { Layout } from "~/components/layout/Layout";
 export default component$(() => {
-  // Hopefully will get a nice dashboard going once server actions are here but until then
-  // it's a pain to draw data from different sources
   return (
-    <>
-      <div>Coming soon!</div>
-      <a href="/">Click here to return to your account</a>
-    </>
+    <Layout>
+      <Heading>Coming soon!</Heading>
+      <Button link="/">Click here to return to your account</Button>
+    </Layout>
   );
 });
