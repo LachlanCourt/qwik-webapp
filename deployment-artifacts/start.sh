0cd47853a11f84550b85a171de6ae88d9b184b47
LachlanCourt <lachlan.m.court@gmail.com>
2023-06-12 20:32:20 +1000

    .

--- deployment-artifacts/start.sh
+++ deployment-artifacts/start.sh
@@ -6,6 +6,7 @@ node node_modules/prisma/build migrate reset --force
 # Run pending migrations
 node add-seed-migration.js
 node node_modules/prisma/build migrate deploy
+rm -rf $(find . -type d -name "*seed_user*")
 
 # Start application
 node server/entry.express
