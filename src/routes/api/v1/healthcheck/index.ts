96fa5c4c3c63063bce0695efd0a44e7a9e72d0f1
LachlanCourt <lachlan.m.court@gmail.com>
2023-06-18 13:26:58 +1000

    Add curl to docker file

--- src/routes/api/v1/healthcheck/index.ts
+++ src/routes/api/v1/healthcheck/index.ts
@@ -1,5 +1,5 @@
 import { RequestHandler } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = async (requestEvent) => {
-    requestEvent.json(200, null)
-}
+  requestEvent.json(200, { status: "Healthy" });
+};
