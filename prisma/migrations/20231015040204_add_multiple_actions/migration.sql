c7b0b2c595481a2eb654450a7c76276e63dd54da
LachlanCourt <lachlan.m.court@gmail.com>
2023-10-15 15:49:33 +1100

    Add new migration and change logic around inserting global admins

---
+++ prisma/migrations/20231015040204_add_multiple_actions/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "Action" (
+    "id" SERIAL NOT NULL,
+    "commandId" INTEGER NOT NULL,
+    "type" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+
+    CONSTRAINT "Action_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Action" ADD CONSTRAINT "Action_commandId_fkey" FOREIGN KEY ("commandId") REFERENCES "Command"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- Copy data from command into action
+INSERT INTO "Action" ("commandId", "type", "content") 
+SELECT id, 'RESPONSE', response FROM "Command";
+
+
+-- AlterTable
+ALTER TABLE "Command" DROP COLUMN "response";
