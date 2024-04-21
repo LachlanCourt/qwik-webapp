b8c6c69c441e431bfb867aa22fd4f4b9cb55a53b
LachlanCourt <lachlan.m.court@gmail.com>
2024-04-21 17:22:01 +1000

    .

--- .github/workflows/yarn-audit.sh
+++ .github/workflows/yarn-audit.sh
@@ -4,7 +4,7 @@ yarn audit
 audit_exit_code=$?
 
 echo yarn audit exited with code $audit_exit_code
-if [ $audit_exit_code -le 6 ]; then
+if [ $audit_exit_code -le 6 ]; then # 6 allows for Info, Low and Moderate and will fail on High and Critical
     exit 0
 else
     exit 1
