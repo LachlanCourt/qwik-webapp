ed700b9028960cde988412d498322e453994d16c
LachlanCourt <lachlan.m.court@gmail.com>
2024-04-21 17:20:44 +1000

    .

--- .github/workflows/yarn-audit.sh
+++ .github/workflows/yarn-audit.sh
@@ -3,8 +3,8 @@
 yarn audit
 audit_exit_code=$?
 
+echo yarn audit exited with code $audit_exit_code
 if [ $audit_exit_code -le 6 ]; then
-    echo yarn audit exited with code $audit_exit_code
     exit 0
 else
     exit 1
